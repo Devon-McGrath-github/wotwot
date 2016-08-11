c434f1dde22876426d8e9a189a4a3645229944dd
Devon McGrath <devon.mcgrath@gmail.com>
2016-08-11 15:03:01 +1200

    semantic properly installed. Need to look into npm i issues, not installing all dependencies as it should

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -10,36 +10,36 @@ export default (props) => {
       <div className="ui grid">
         <div className="four wide column">
           <div className="ui vertical fluid tabular menu">
-            <a className="active item">
-              <Link to='/' className="active item">
+            <a className="item active">
+              <Link to='/'>
                 <p>WotWot</p>
               </Link>
             </a>
             <a className="item">
-              <Link to="event-list" className="item" onClick={() => props.getActivities()}>
+              <Link to="event-list" onClick={() => props.getActivities()}>
                 <p>Event List</p>
               </Link>
             </a>
             <a className="item">
               {signedIn ?
-                <Link to="new-event" className="item">
+                <Link to="new-event">
                   <p>Create Event</p>
                 </Link> :
-                <a href="/login.html" className="item"><p>Create Event</p></a>
+                <a href="/login.html"><p>Create Event</p></a>
               }
             </a>
             <a className="item">
-              {signedIn ? <button className="ui item">Welcome: {props.auth.username}</button> : ''}
+              {signedIn ? <button>Welcome: {props.auth.username}</button> : ''}
               {signedIn ?
-                <button className="ui item" onClick={props.logoutUser}>Logout</button> :
-                <a href="/login.html" className="ui item">Sign In | Sign up</a>}
+                <button onClick={props.logoutUser}>Logout</button> :
+                <a href="/login.html">Sign In | Sign up</a>}
             </a>
           </div>
         </div>
         <div className="twelve wide stretched column">
-        <div class="ui segment">
-          <p>This is an stretched grid column. This segment will always match the tab height</p>
-        </div>
+          <div class="ui segment">
+            <p>This is an stretched grid column. This segment will always match the tab height</p>
+          </div>
         </div>
       </div>
     </div>
