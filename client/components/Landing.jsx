259173d3c886713b09c7465c0abdfd0916f69b27
Devon McGrath <devon.mcgrath@gmail.com>
2016-08-11 13:43:05 +1200

    updated semantic to appropriate jsx code
    semantic wasnt working as I was using class=... instead of className=... which is necessary for jsx html

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -8,27 +8,6 @@ export default (props) => {
   return (
   <div className="hero-image">
     <Nav />
-    <div class="twelve wide stretched column">
-      <div class="ui segment">
-        <div className="hero-container">
-          <div className="hero-header">
-            <h1 className="hero-h1">WotWot</h1>
-          </div>
-
-          <div className="hero-description">
-            <h3 className="hero-h3"><span className="hero-desc-text">The Personality Hub</span></h3>
-          </div>
-
-          <div className="hero-button">
-            <Link to='event-list'>
-              <button className="ui inverted black button">
-                <span className="events-list-text">Events Near Me</span>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
   )
 }
